ded0a20464051ee42b379aecfc7c2e85c9cea4f7
Bob Steers <Bob Steers>
2017-11-02 11:09:02 +0100

    add eventlistener

--- indexing_experiment/rangefinder/__main__.py
+++ indexing_experiment/rangefinder/__main__.py
@@ -40,7 +40,7 @@ def get_args():
 
 def init_logging(args):
     log_args = {
-        'level': logging.INFO,
+        'level': logging.DEBUG,
         'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
         'datefmt': '%m-%d %H:%M:%S'
     }
--- indexing_experiment/rangefinder/connector.py
+++ indexing_experiment/rangefinder/connector.py
@@ -2,14 +2,38 @@
 Responsible for connecting to the database, and creating the session object
 """
 
+import time
+import logging
 import sqlalchemy
+from sqlalchemy import event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from contextlib import contextmanager
 from rangefinder import models
 
 
 Session = sessionmaker()
+
+
+@event.listens_for(Engine, 'before_cursor_execute')
+def before_cursor_execute(conn, cursor, statement,
+                          parameters, context, executemany):
+    context._query_start_time = time.time()
+    logging.debug('Start Query:\n%s' % statement)
+    # Modification for StackOverflow answer:
+    # Show parameters, which might be too verbose, depending on usage..
+    logging.debug('Parameters:\n%r' % (parameters,))
+
+
+@event.listens_for(Engine, 'after_cursor_execute')
+def after_cursor_execute(conn, cursor, statement,
+                         parameters, context, executemany):
+    total = time.time() - context._query_start_time
+    logging.debug('Query Complete!')
+
+    # Modification for StackOverflow: times in milliseconds
+    logging.debug('Total Time: %.02fms' % (total * 1000))
 
 
 def connect(user, password, db, host='localhost', port=5432):
