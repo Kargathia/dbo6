df018ab12f814694781366c458c99937fa5d27ef
Bob Steers <Bob Steers>
2017-11-02 12:46:46 +0100

    neatify

--- indexing_experiment/rangefinder/finder.py
+++ indexing_experiment/rangefinder/finder.py
@@ -11,12 +11,7 @@ from rangefinder.explainer import Explain
 
 
 def find_closest(session, lat, long, limit):
-    # Calculating each zipcode range's distance from Target
     radius = 100
-    loc_target = func.ll_to_earth(lat, long)
-    loc_ziprange = func.ll_to_earth(ZipCodeRange.latitude, ZipCodeRange.longitude)
-    # distance_func = func.earth_box(func.ll_to_earth(loc_target, loc_ziprange), 100)
-
     query = session.query(ZipCodeRange).filter(
         func.earth_box(func.ll_to_earth(lat, long), radius
                        ).op('@>')(func.ll_to_earth(ZipCodeRange.latitude, ZipCodeRange.longitude))
@@ -29,9 +24,7 @@ def find_closest(session, lat, long, limit):
     analysis = session.execute(Explain(query, analyze=True), query_params).fetchall()
     logging.info(pformat(analysis))
 
-    # Resultset is no longer list of Ranges, but a list of tuples.
     result = query.all()
-
     [logging.info('found zipcoderange(zipcode={}, lat={},long={})'.format(
         r.zipcode_code,
         r.latitude,
